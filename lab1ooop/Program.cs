aa65a2978217c9b03f5c9984ac39fa85a08b6cb9
l1e <l1e@DESKTOP-9K58KRP>
2024-02-27 21:09:43 +0200

    Добавьте файлы проекта.

---
+++ lab1ooop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab1oop1
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+
+        static void Main(string[] args)
+        {
+
+            MyDelegate myDelegate1 = new MyDelegate(Timer.Method1);
+            MyDelegate myDelegate2 = new MyDelegate(Timer.Method2);
+
+      
+            Thread thread1 = new Thread(new ThreadStart(myDelegate1.Invoke));
+            Thread thread2 = new Thread(new ThreadStart(myDelegate2.Invoke));
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+        }
+
+      
+    }
+}
---
+++ lab1ooop/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Lab1oop1
+{
+    class Timer
+    {
+        public static void Method1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"номер потоку: {Thread.CurrentThread.ManagedThreadId} - {DateTime.Now} - Method1 виконаний");
+               
+            }            
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine($"номер потоку: {Thread.CurrentThread.ManagedThreadId} - {DateTime.Now} - Method2 виконаний");
+                
+            } 
+        }
+    }
+}
